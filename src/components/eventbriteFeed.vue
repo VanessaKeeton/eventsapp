4363e00c2edf3280ed02b48ec0c4aa324e4ce0f0
Vanessa Keeton <innerurge1@gmail.com>
2022-01-07 22:13:15 -0500

    refactor: clean up formatting and url params to params object

--- src/components/eventbriteFeed.vue
+++ src/components/eventbriteFeed.vue
@@ -1,20 +1,20 @@
 <template>
-    <ev-header :semanticLevel="1" displayAs="heading1">Events</ev-header>
-    <ul :class="$style.eventList">
-      <li :class="$style.eventListItem" v-for="event in events" :key="event.id">
-        <h2 :class="$style.eventDate">{{ prettyDate(event.start.local) }}</h2>
-        <img :src="event.logo.url" alt="">
-        <ev-header :semanticLevel="3" displayAs="heading2">{{ event.name.text }}</ev-header>
-        <p :class="$style.eventDescription">{{ event.description.text }}</p>
-        <address>
-          <span>{{ event.venue.name }}</span><br>
-          <span v-if="event.venue.address.address_1">{{ event.venue.address.address_1 }}</span><br>
-          <span v-if="event.venue.address.address_2">{{ event.venue.address.address_2 }}</span>
-        </address>
-        <div :class="$style.purchaseLinkContainer"><ev-link :href="`${event.url}?aff=website`" target="_blank" asButton>Purchase Tickets</ev-link></div>
-      </li>
-      <li v-if="loading">loading...</li>
-    </ul>
+  <ev-header :semanticLevel="1" displayAs="heading1">Events</ev-header>
+  <ul :class="$style.eventList">
+    <li :class="$style.eventListItem" v-for="event in events" :key="event.id">
+      <h2 :class="$style.eventDate">{{ prettyDate(event.start.local) }}</h2>
+      <img :src="event.logo.url" alt="">
+      <ev-header :semanticLevel="3" displayAs="heading2">{{ event.name.text }}</ev-header>
+      <p :class="$style.eventDescription">{{ event.description.text }}</p>
+      <address>
+        <span>{{ event.venue.name }}</span><br>
+        <span v-if="event.venue.address.address_1">{{ event.venue.address.address_1 }}</span><br>
+        <span v-if="event.venue.address.address_2">{{ event.venue.address.address_2 }}</span>
+      </address>
+      <div :class="$style.purchaseLinkContainer"><ev-link :href="`${event.url}?aff=website`" target="_blank" asButton>Purchase Tickets</ev-link></div>
+    </li>
+    <li v-if="loading">loading...</li>
+  </ul>
 </template>
 
 <script>
@@ -25,7 +25,7 @@ import EvLink from './link'
 import prettyDate from '../filters/prettyDate'
 
 export default {
-  name: 'eventbrightFeed',
+  name: `eventbrightFeed`,
   components: {
     EvHeader,
     EvLink
@@ -33,12 +33,20 @@ export default {
   setup() {
     const events = ref(null);
     const loading = ref(true);
+    const token = `S7R32KSQEZUKBJS4O4HI`;
+    const organizationId = `260888089976`;
 
-    const eventsToRequest = function () {
+    const eventsToRequest = () => {
       return new Promise((resolve, reject) => {
       axios({
-        method: 'get',
-        url: 'https://www.eventbriteapi.com/v3/organizations/260888089976/events/?expand.event=organizer,venue&status=live&time_filter=current_future&token=S7R32KSQEZUKBJS4O4HI&page_size=50',
+        method: `get`,
+        params: {
+          status:`live`,
+          time_filter:`current_future`,
+          token:token,
+          page_size:`50`
+        },
+        url: `https://www.eventbriteapi.com/v3/organizations/${organizationId}/events/`,
       }).then(data => {
             const events = data.data.events.map(event=>event.id)
             resolve(events)
@@ -50,12 +58,16 @@ export default {
     }
 
     const getEvents = async ()=> {
-      loading.value = true
+      loading.value = true;
       const getList = await eventsToRequest();
       axios({
-          method: 'get',
-          url: `https://www.eventbriteapi.com/v3/events/?token=S7R32KSQEZUKBJS4O4HI&event_ids=${getList.join(',')}
-          &expand=venue`
+          method: `get`,
+          url: `https://www.eventbriteapi.com/v3/events/`,
+          params: {
+            token: token,
+            event_ids: getList.join(`,`),
+            expand:"venue"
+          }
         }).then(data => {
           const sortedEvents = data.data.events.sort((a, b) => (new Date(a.start.local) - new Date(b.start.local)))
           events.value = sortedEvents
@@ -76,28 +88,28 @@ export default {
 </script>
 
 <style lang="scss" module>
-  .eventListItem:not(last) {
-    margin-bottom: 1.66em;
-    border-bottom: 1px solid #ccc;
-    padding-bottom: 16px;
-  }
-  .eventDescription {
-    line-height: 1.25;
-    margin-bottom: 1em;
-    font-size: 12px;
-    font-weight: 400;
-    letter-spacing: .25pt;
-  }
-  .eventDate {
-    margin-bottom: .5em;
-    font-size: 14px;
-    color: rgb(223, 139, 29);
-    font-weight: 400;
-    letter-spacing: .25pt;
-  }
-  .purchaseLinkContainer {
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-end;
-  }
+.eventListItem:not(last) {
+  margin-bottom: 1.66em;
+  border-bottom: 1px solid #ccc;
+  padding-bottom: 16px;
+}
+.eventDescription {
+  line-height: 1.25;
+  margin-bottom: 1em;
+  font-size: 12px;
+  font-weight: 400;
+  letter-spacing: .25pt;
+}
+.eventDate {
+  margin-bottom: .5em;
+  font-size: 14px;
+  color: rgb(223, 139, 29);
+  font-weight: 400;
+  letter-spacing: .25pt;
+}
+.purchaseLinkContainer {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+}
 </style>
